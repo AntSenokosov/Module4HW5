9f94af531e9d78987588883133158d849a483486
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-27 20:39:12 +0200

    Add App

---
+++ Module4HW5/Module4HW5/App.cs
@@ -0,0 +1,20 @@
+﻿using Module4HW5.Helpers;
+
+namespace Module4HW5;
+
+public class App
+{
+    public async Task Run(string[] args)
+    {
+        await using (var context = new SampleContextFactory().CreateDbContext(args))
+        {
+            var query = new Query(context);
+            await TransactionClass.Transaction(() => query.JoinTables(), args);
+            await TransactionClass.Transaction(() => query.DateDiffQuery(), args);
+            await TransactionClass.Transaction(() => query.UpdateEntities(), args);
+            await TransactionClass.Transaction(() => query.AddEntityEmployee(), args);
+            await TransactionClass.Transaction(() => query.DeleteEntityEmployee(), args);
+            await TransactionClass.Transaction(() => query.GroupEmployee(), args);
+        }
+    }
+}
