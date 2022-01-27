3aac4258a1ff0136357e342ffdd0fc3dc750954c
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-27 18:29:47 +0200

    Add TransactionClass

---
+++ Module4HW5/Module4HW5/Helpers/TransactionClass.cs
@@ -0,0 +1,22 @@
+﻿namespace Module4HW5.Helpers;
+
+public class TransactionClass
+{
+    public async Task Transaction(Func<Task> func, string[] args)
+    {
+        await using (var transaction =
+                     await new SampleContextFactory().CreateDbContext(args).Database.BeginTransactionAsync())
+        {
+            try
+            {
+                await func();
+                await transaction.CommitAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                await transaction.RollbackAsync();
+            }
+        }
+    }
+}
