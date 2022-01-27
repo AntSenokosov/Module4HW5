0c734b95f626414be945d122390603444bf8ad0a
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-27 18:16:19 +0200

    Add query DateDiffQuery

--- Module4HW5/Module4HW5/Helpers/Query.cs
+++ Module4HW5/Module4HW5/Helpers/Query.cs
@@ -1,4 +1,6 @@
-﻿namespace Module4HW5.Helpers;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Module4HW5.Helpers;
 
 public class Query
 {
@@ -7,5 +9,14 @@ public class Query
     public Query(ApplicationContext context)
     {
         _context = context;
+    }
+
+    // Запрос, который возвращает разницу между CreatedDate/HiredDate и сегодня. Фильтрация должна быть выполнена на сервере.
+    public async Task DateDiffQuery()
+    {
+        var data = await _context.Employees
+            .AsNoTracking()
+            .Select(s => s.HiredDate)
+            .ToListAsync();
     }
 }
