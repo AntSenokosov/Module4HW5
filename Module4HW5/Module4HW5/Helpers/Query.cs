0def6bdaddf620c52029f268a4668a004019551b
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-27 16:44:25 +0200

    Add class Query

---
+++ Module4HW5/Module4HW5/Helpers/Query.cs
@@ -0,0 +1,11 @@
+﻿namespace Module4HW5.Helpers;
+
+public class Query
+{
+    private readonly ApplicationContext _context;
+
+    public Query(ApplicationContext context)
+    {
+        _context = context;
+    }
+}
